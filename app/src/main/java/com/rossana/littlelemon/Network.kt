57e7f37441b4fe9b679e00877b64942dd51ac2fa
Rossana <30724184+rossanafmenezes@users.noreply.github.com>
2023-11-06 07:43:26 -0500

    Add Network file

---
+++ app/src/main/java/com/rossana/littlelemon/Network.kt
@@ -0,0 +1,23 @@
+package com.rossana.littlelemon
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class MenuNetwork(
+    val menu: List<MenuItemNetwork>
+)
+
+@Serializable
+data class MenuItemNetwork(
+    @SerialName("id")
+    val id: Int,
+    @SerialName("title")
+    val title: String,
+    @SerialName("price")
+    val price: Double,
+    @SerialName("description")
+    val description: String,
+    @SerialName("image")
+    val image: String
+)
