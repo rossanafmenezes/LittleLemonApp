e2470ce6e1380bb0a1f2e5508d80decc2e80a7f3
Rossana <30724184+rossanafmenezes@users.noreply.github.com>
2023-11-05 19:47:27 -0500

    Add destination routes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.3.1")
+    implementation("androidx.navigation:navigation-compose:2.4.0-alpha10")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
--- app/src/main/java/com/rossana/littlelemon/Destinations.kt
+++ app/src/main/java/com/rossana/littlelemon/Destinations.kt
@@ -1,2 +1,17 @@
 package com.rossana.littlelemon
 
+interface Destinations {
+    val route: String
+}
+
+object Onboarding : Destinations {
+    override val route = "onboarding"
+}
+
+object Home : Destinations {
+    override val route = "home"
+}
+
+object Profile : Destinations {
+    override val route = "profile"
+}
