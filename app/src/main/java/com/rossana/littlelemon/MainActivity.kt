3875b4e010393e0eff6a3594eb906d46e85c846f
Rossana <30724184+rossanafmenezes@users.noreply.github.com>
2023-11-06 07:56:18 -0500

    Add database variable and function to save all

--- app/src/main/java/com/rossana/littlelemon/MainActivity.kt
+++ app/src/main/java/com/rossana/littlelemon/MainActivity.kt
@@ -4,13 +4,44 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.navigation.compose.rememberNavController
+import androidx.room.Room
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.serialization.kotlinx.json.json
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.http.ContentType
 
 class MainActivity : ComponentActivity() {
+
+    private val httpClient = HttpClient(Android) {
+        install(ContentNegotiation) {
+            json(contentType = ContentType("text", "plain"))
+        }
+    }
+
+    private val database by lazy {
+        Room.databaseBuilder(applicationContext, AppDatabase::class.java, "database").build()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             val navController = rememberNavController()
             Navigation(navController, context = this)
         }
+    }
+
+    private suspend fun fetchMenu(): List<MenuItemNetwork> {
+        val response =
+            httpClient
+                .get("https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu.json")
+                .body<MenuNetwork>()
+        return response.menu
+    }
+
+    private fun saveMenuToDataBase(menuItemsNetwork: List<MenuItemNetwork>) {
+        val menuItemsRoom = menuItemsNetwork.map { it.toMenuItemRoom() }
+        database.menuItemDao().insertAll(*menuItemsRoom.toTypedArray())
     }
 }
