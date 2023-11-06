54c2897ec83452330589f256388ca6612d37b9a6
Rossana <30724184+rossanafmenezes@users.noreply.github.com>
2023-11-05 21:23:23 -0500

    Add logic to start the app based on if user is logged or not

--- app/src/main/java/com/rossana/littlelemon/NavigationComposable.kt
+++ app/src/main/java/com/rossana/littlelemon/NavigationComposable.kt
@@ -1,19 +1,25 @@
 package com.rossana.littlelemon
 
+import android.content.Context
 import androidx.compose.runtime.Composable
-
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 
 @Composable
-fun Navigation(navController: NavHostController) {
-    NavHost(navController = navController, startDestination = Onboarding.route) {
+fun Navigation(navController: NavHostController, context: Context) {
+    val sharedPref = context.getSharedPreferences("user_info", Context.MODE_PRIVATE)
+    val startDestination = if(sharedPref.getBoolean("onboarding_complete", true)) {
+        Home.route
+    } else {
+        Onboarding.route
+    }
+    NavHost(navController = navController, startDestination = startDestination) {
         composable(Onboarding.route) {
             Onboarding(navController)
         }
         composable(Home.route) {
-            Home()
+            Home(navController)
         }
         composable(Profile.route) {
             Profile(navController)
